098e422104f1613d483c434622c18580f60c6d11
timomasic <tmasic@udesa.edu.ar>
2021-04-13 13:47:00 -0300

    fake data

---
+++ data/index.js
@@ -0,0 +1,85 @@
+let instrumentos = {
+    lista: [ 
+    {id: 1,
+    nombre: "Trompeta",
+    fechaDeCreacion: "2/3/2021",
+    imagen: "https://ar.xprostore.com/image/cache/data/imagenesweb/ytr2330-650x650.jpg" },
+
+    {id: 2,
+    nombre: "Trombón",
+    fechaDeCreacion: "3/3/2021",
+    imagen: "https://ar.xprostore.com/image/cache/data/imagenesweb/ysl154-650x650.jpg" },
+
+    {id: 3,
+    nombre: "Clarinete",
+    fechaDeCreacion: "4/3/2021",
+    imagen: "https://ar.xprostore.com/image/cache/data/imagenesweb/ycl255-650x650.jpg" },
+
+    {id: 4,
+    nombre: "Guitarra Electrica",
+    fechaDeCreacion: "5/3/2021",
+    imagen: "https://famauy.xprostore.com/image/cache/data/imagenesweb/lps18odch1-650x650.jpg" },
+
+    {id: 5,
+    nombre: "Bajo",
+    fechaDeCreacion: "6/3/2021",
+    imagen: "https://ar.xprostore.com/image/cache/data/imagenesweb/gb34jj-3ts-650x650.jpg" },
+
+    {id: 6,
+    nombre: "Bateria",
+    fechaDeCreacion: "7/3/2021",
+    imagen: "https://ar.xprostore.com/image/cache/data/imagenesweb/rli52kh4sw-650x650.jpg" },
+
+    {id: 7,
+    nombre: "Violin",
+    fechaDeCreacion: "8/3/2021",
+    imagen: "https://www.yulmar.com.ar/fotos/articulos/1661333415-Violin-Estudio-Stradella-1-2-Estuche-Resina-Arco.jpg" },
+
+    {id: 8,
+    nombre: "Guitarra Acústica",
+    fechaDeCreacion: "9/3/2021",
+    imagen: "https://ar.xprostore.com/image/cache/data/imagenesweb/earth100-ns-650x650.jpg" },
+
+    {id: 9,
+    nombre: "Flauta Dulce",
+    fechaDeCreacion: "10/3/2021",
+    imagen: "https://ar.xprostore.com/image/cache/data/imagenesweb/yrs20g-b-650x650.jpg" },
+
+    {id: 10,
+    nombre: "Flauta Traversa",
+    fechaDeCreacion: "11/3/2021",
+    imagen: "https://www.yulmar.com.ar/fotos/articulos/1793420292-Flauta-Traversa-Wisemann-Dfl-310-Plateada-16-Llaves-Estuche.jpg" },
+
+    {id: 11,
+    nombre: "Fagot",
+    fechaDeCreacion: "12/3/2021",
+    imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPX4XWY8VTR5OXlxCiGbHrkMakqMcWKZ4m5C5P1KP7xg61_R3rVE5m55QMV8I0n75uWhk&usqp=CAU" },
+
+    {id: 12,
+    nombre: "Tambor",
+    fechaDeCreacion: "13/3/2021",
+    imagen: "https://ar.xprostore.com/image/cache/data/imagenesweb/taj-12-t2-650x650.jpg" },
+
+    {id: 13,
+    nombre: "Maracas",
+    fechaDeCreacion: "14/3/2021",
+    imagen: "https://cl.xprostore.com/image/cache/data/imagenesweb/tmp-b-650x650.jpg" },
+
+    {id: 14,
+    nombre: "Triangulo",
+    fechaDeCreacion: "15/3/2021",
+    imagen: "https://famauy.xprostore.com/image/cache/data/imagenesweb/cr-p006-00-650x650h.jpg" },
+
+    {id: 15,
+    nombre: "Arpa",
+    fechaDeCreacion: "16/3/2021",
+    imagen: "https://www.cdbp.patrimoniocultural.gob.cl/652/articles-79173_imagen_06.jpg" },
+
+    {id: 16,
+    nombre: "Ukelele",
+    fechaDeCreacion: "17/3/2021",
+    imagen: "https://ar.xprostore.com/image/cache/data/imagenesweb/ma-s-650x650.jpg" },
+    ],
+}
+
+module.exports = instrumentos
