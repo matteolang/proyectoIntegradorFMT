bac0d353322c0747224fe641648bc2f83745885d
matteolang <mlang@udesa.edu.ar>
2021-04-18 01:28:26 -0300

    buscador adaptado a nombres marcas y modelos

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,5 @@
 let instrumentos = require('../data/index')
+const { json } = require('express')
 
 let mainController = {
     index: (req, res) => {
@@ -10,11 +11,16 @@ let mainController = {
 
         let result = []
         for( let i = 0; i < instrumentos.lista.length; i++){
+            var nombreMarcaModelo = instrumentos.lista[i].nombreDelInstrumento.toLowerCase() + ' ' + instrumentos.lista[i].marca.toLowerCase() + ' ' + instrumentos.lista[i].modelo.toLowerCase()
+            var nombreModelo = instrumentos.lista[i].nombreDelInstrumento.toLowerCase() + ' ' + instrumentos.lista[i].modelo.toLowerCase()
             if(instrumentos.lista[i].nombreDelInstrumento.toLowerCase().includes(search.toLowerCase()) || instrumentos.lista[i].marca.toLowerCase().includes(search.toLowerCase()) || instrumentos.lista[i].modelo.toLowerCase().includes(search.toLowerCase())){
                 result.push(instrumentos.lista[i])
             }
+            else if(nombreMarcaModelo.includes(search.toLowerCase()) || nombreModelo.includes(search.toLowerCase())){
+                result.push(instrumentos.lista[i])
+            }
         }
-        res.render('search-results', {resultadoSearch: result, instrumentitos: instrumentos.lista})
+        res.render('search-results', {resultadoSearch: result, instrumentitos: instrumentos.lista, parametroSearch: search})
     },
 }
 
